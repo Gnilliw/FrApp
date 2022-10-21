fbd57e33d138ddced07d5cf4a6f75baff6497903
f.willing <f.willing@conterra.de>
2022-10-21 14:20:00 +0200

    weatherBundle

---
+++ src/main/js/bundles/weather/weatherFactory.js
@@ -0,0 +1,9 @@
+/*
+import Vue from "apprt-vue/Vue";
+import VueDijit from "apprt-vue/VueDijit";
+import weatherVue from "./weather.vue";
+
+let weatherVue = new Vue(weatherVue);
+
+let weatherWidget = VueDijit(weatherVue);
+*/
